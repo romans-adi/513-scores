d58267e3848ed1aa406160953f1f59ba3d0fb33c
Romans S <romans.spilaks@gmail.com>
2023-07-01 17:25:16 +0300

    Add Home.test.js to check if Home page renders properly

--- src/components/Indicators/LoadingIndicator.jsx
+++ src/components/Indicators/LoadingIndicator.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 
 const LoadingIndicator = () => (
-  <ReactLoading type="bubble" color="#4369B2" height="667px" width="375px" />
+  <ReactLoading data-testid="loading-indicator" type="bubble" color="#4369B2" height="667px" width="375px" />
 );
 
 export default LoadingIndicator;
